54f7a29f607ffe511acca7f86178173d62cbdb9f
Christian Heller <c.heller@plomlompom.de>
2012-02-03 04:04:01 +0100

    Moved in BuildPostForm() removed from PlomWiki repository.

--- StandardLib.php
+++ StandardLib.php
@@ -10,3 +10,15 @@ function GetAllPageTitles()
       $titles[] = $fn;
   closedir($p_dir); 
   return $titles; }
+
+function BuildPostForm($URL, $input, $ask_pw = NULL, $class = NULL)
+# HTML form. $URL = action, $input = code between, $ask_pw = PW input element.
+{ global $esc, $nl;
+  if ($ask_pw === NULL)
+    $ask_pw = 'Admin '.$esc.'pw'.$esc.': <input name="pw" type="password" />'.
+                              '<input name="auth" type="hidden" value="*" />';
+  if ($class !== NULL)
+    $class = 'class="'.$class.'" ';
+  return '<form '.$class.'method="post" action="'.$URL.'">'.$nl.$input.$nl.
+                                                                    $ask_pw.$nl.
+         '<input type="submit" value="OK" />'.$nl.'</form>'; }
