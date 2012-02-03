c214b625c93226ab8323baf600b49f5c55ade8df
Christian Heller <c.heller@plomlompom.de>
2012-02-03 03:24:38 +0100

    Moved ChangePW_form() out of plomwiki.php and into the only plugin using it still: PwPages.php

--- PwPages.php
+++ PwPages.php
@@ -17,3 +17,17 @@ $l['page'] = 'page';
 function Action_page_set_pw()
 { global $esc, $title;
   ChangePW_form($esc.'page'.$esc.' "'.$title.'"', $title); }
+
+function ChangePW_form($desc_new_pw, $new_auth, $desc_pw = 'Admin', 
+                       $auth = '*', $t = 'admin_sets_pw')
+# Output page for changing password keyed to $auth and described by $desc.
+{ global $esc, $l, $nl, $nl2, $title_url;
+  $input = $esc.'NewPWfor'.$esc.' '.$desc_new_pw.':<br />'.$nl.
+           '<input type="hidden" name="new_auth" value="'.$new_auth.'">'.$nl
+          .'<input type="password" name="new_pw" /><br />'.$nl.
+           '<input type="hidden" name="auth" value="'.$auth.'">'.$nl.
+           $desc_pw.' '.$esc.'pw'.$esc.':<br />'.$nl.
+           '<input type="password" name="pw">';
+  $form = BuildPostForm($title_url.'&amp;action=write&amp;t='.$t, $input, '');
+  $l['title'] = $esc.'ChangePWfor'.$esc.' '.$desc_new_pw; $l['content'] = $form;
+  OutputHTML(); }
