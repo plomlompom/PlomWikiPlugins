914d4c4aa96305c3c67f22137e9efd9ffb1f801a
Christian Heller <c.heller@plomlompom.de>
2012-02-10 01:47:55 +0100

    Better comments in AutoSum.php.

--- AutoSum.php
+++ AutoSum.php
@@ -1,38 +1,50 @@
 <?php
-# Provide a more sophisticated default $summary for diffs.
+# PlomWiki plugin: AutoSum
+# 
+# Provides to the diff summary field an automatic summary of what was changed,
+# by excerpting the actual diff text.
 
+# Hook into WritePage() to fill up an empty $summary with AutoSum().
 $hook_WritePage_diff .= 'if (!$summary) $summary = AutoSum($diff_add);';
 
 function AutoSum($diff_add)
-# Build a nice short summary of a diff from $diff_add.
+# Build a nice short single-line summary of a diff from diff text provided.
 { global $nl;
   $diff_lines = explode($nl, $diff_add); 
 
+  # Count "<" and ">" lines.
   $n_add = 0; $n_del = 0;
   foreach ($diff_lines as $line)
   { if     ('>' == $line[0]) $n_add++;
     elseif ('<' == $line[0]) $n_del++; }
 
+  # Build summary of ">" lines.
   if (0 < $n_add)
   { $sep_to_minus = ' ';
-    $prefix_add = '[+]';
+    $prefix_add   = '[+]';
     foreach ($diff_lines as $line)
     { if ('>' == $line[0])
       { $string_add .= $sep.substr($line, 1); 
-        $sep = ' / '; } 
-      else $sep = ' […] '; } }
+        $sep         = ' / '; } 
+      else
+        $sep         = ' […] '; } }
   $string_add = substr($string_add, 6);
 
+  # Build summary of "<" lines.
   if (0 < $n_del)
   { $prefix_del = '[-]';
     foreach ($diff_lines as $line)
     { if ('<' == $line[0])
       { $string_del .= $sep.substr($line, 1); 
-        $sep = ' / '; } 
-      else $sep = ' […] '; } }
+        $sep         = ' / '; } 
+      else
+        $sep         = ' […] '; } }
   $string_del = substr($string_del, 6);
 
-  $summary = 'AutoSum: '.$prefix_add.$string_add.$sep_to_minus.$prefix_del.$string_del;
+  # Build whole summary string by adding everything; cut down to 100 UTF8 chars.
+  $summary = 'AutoSum: '.$prefix_add.$string_add.$sep_to_minus.$prefix_del.
+                                                                    $string_del;
   if (100 < strlen($summary))
-    $summary = mb_substr($summary, 0, 99, 'UTF-8').'…'; 
+    $summary = mb_substr($summary, 0, 99, 'UTF-8').'…';
+  
   return $summary; }
