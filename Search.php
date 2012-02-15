8437f44d7c3a2c054a45431a00608a116a5d2c59
Christian Heller <c.heller@plomlompom.de>
2012-02-15 04:24:28 +0100

    Changed Search.php to new registry system.

--- Search.php
+++ Search.php
@@ -1,44 +1,44 @@
 <?php
 # PlomWiki plugin: Search
 # 
-# Provides a very simple case-insensitive full-text string search through all
-# pages of a PlomWiki via Action_Search().
+# Provides a very simple case-insensitive full-text string search
+# through all pages of a PlomWiki via Action_Search().
 
-# Language-specific variables.
-$l['Search']            = 'Search'; 
-$l['SearchResults']     = 'Search results for';
-$l['SearchResultsNone'] = 'None.';
+$l = ReadReg($plugin_regs_dir.'Search', $l);
 
-function Action_Search()
+function Action_Search() {
 # Case-insensitive search through all pages' texts and titles.
-{ global $esc, $legal_title, $l, $nl, $nl2, $pages_dir, $title_root;
+  global $l, $nl, $pages_dir;
 
   # Produce search results HTML if $_GET['query'] is provided.
   $query = $_GET['query'];
-  if ($query)
-  { if (get_magic_quotes_gpc())
+  if ($query) {
+    if (get_magic_quotes_gpc())
       $query = stripslashes($query);
+    $l['Search_query']       = $query;
+    $l['Search_queryNoHTML'] = EscapeHTML($query);
 
-    $results = $nl2.
-               '<h2>'.$esc.'SearchResults'.$esc.': '.EscapeHTML($query).'</h2>'.
-                                                                           $nl2;
-
-    $matches = array();
+    # Collect titles of pages matching $query into $matches, formatted.
+    $matches   = array();
     $query_low = strtolower($query);
-    foreach (GetAllPageTitles() as $title)
-    { $content_low = strtolower(file_get_contents($pages_dir.$title));
-      if (strstr($content_low, $query_low) 
-          or strstr(strtolower($title), $query_low)) 
-        $matches[]='<li><a href="'.$title_root.$title.'">'.$title.'</a></li>'; }
+    foreach (GetAllPageTitles() as $title) {
+      $content_low = strtolower(file_get_contents($pages_dir.$title));
+      if (strstr($content_low, $query_low)
+          or strstr(strtolower($title), $query_low)) {
+        $l['i_title'] = $title;
+        $matches[]    = ReplaceEscapedVars($l['Search():match']); } }
 
-    $matches_str .= implode($nl, $matches); 
-    if ($matches_str) $results .= '<ul>'.$nl.$matches_str.$nl.'</ul>'; 
-    else              $results .= '<p>'.$esc.'SearchResultsNone'.$esc.'</p>'; }
+    # Format results display depending on matches found or not.
+    $l['Search_Matches'] .= implode($nl, $matches); 
+    if ($l['Search_Matches'])
+      $l['Search():results'] .= $l['Search():ResultsYes'];
+    else
+      $l['Search():results'] .= $l['Search():ResultsNone']; }
 
-  $content = '<form method="get" action="'.$root_rel.'">'.$nl.
-             '<input type="hidden" name="action" value="Search" />'.$nl.
-             '<input type="text" name="query" value="'.$query.'" />'.$nl.
-             '<input type="submit" value="'.$esc.'Search'.$esc.'!" />'.$nl.
-             '</form>'.$results; 
-  $l['title'] = $esc.'Search'.$esc; $l['content'] = $content;
+  # Don't show *any* search results content if no query was provided.
+  else
+    $l['Search():results'] = '';
+
+  $l['title']   = $l['Search():title'];
+  $l['content'] = $l['Search():content'];
   OutputHTML(); }
