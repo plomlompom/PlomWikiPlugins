71f0c908eb3ebc259a5a29446b6e84016cfeccbb
Christian Heller <c.heller@plomlompom.de>
2012-02-15 04:27:41 +0100

    Small formatting change in Today.php.

--- Today.php
+++ Today.php
@@ -1,8 +1,8 @@
 <?php
 # PlomWiki plugin: Today markup
 # 
-# Provides [:today:] markup that translates to a date display of format "Y-m-d".
+# Provides [:today:] markup, displays current date in format "Y-m-d".
 
-function MarkupToday($text)
+function MarkupToday($text) {
 # Translates [:today:] into the current date, formatted as Y-m-d.
-{ return str_replace('[:today:]', date('Y-m-d'), $text); }
+  return str_replace('[:today:]', date('Y-m-d'), $text); }
