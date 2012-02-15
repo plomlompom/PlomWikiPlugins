8e3c087757d8c426058c01f177e6e603ba45873c
Christian Heller <c.heller@plomlompom.de>
2012-02-15 04:32:59 +0100

    Small formatting change in AutoSum.php.

--- AutoSum.php
+++ AutoSum.php
@@ -7,38 +7,38 @@
 # Hook into WritePage() to fill up an empty $summary with AutoSum().
 $hook_WritePage_diff .= 'if (!$summary) $summary = AutoSum($diff_add);';
 
-function AutoSum($diff_add)
+function AutoSum($diff_add) {
 # Build nice short single-line summary of diff from diff text provided.
-{ global $nl;
+  global $nl;
   $diff_lines = explode($nl, $diff_add); 
 
   # Count "<" and ">" lines.
   $n_add = 0; $n_del = 0;
-  foreach ($diff_lines as $line)
-  { if     ('>' == $line[0]) $n_add++;
+  foreach ($diff_lines as $line) {
+    if     ('>' == $line[0]) $n_add++;
     elseif ('<' == $line[0]) $n_del++; }
 
   # Build summary of ">" lines.
-  if (0 < $n_add)
-  { $sep_to_minus = ' ';
+  if (0 < $n_add) {
+    $sep_to_minus = ' ';
     $prefix_add   = '[+]';
     foreach ($diff_lines as $line)
-    { if ('>' == $line[0])
-      { $string_add .= $sep.substr($line, 1); 
+      if ('>' == $line[0]) {
+        $string_add .= $sep.substr($line, 1); 
         $sep         = ' / '; } 
       else
-        $sep         = ' […] '; } }
+        $sep         = ' […] '; }
   $string_add = substr($string_add, 6);
 
   # Build summary of "<" lines.
-  if (0 < $n_del)
-  { $prefix_del = '[-]';
+  if (0 < $n_del) {
+    $prefix_del = '[-]';
     foreach ($diff_lines as $line)
-    { if ('<' == $line[0])
-      { $string_del .= $sep.substr($line, 1); 
+      if ('<' == $line[0]) {
+        $string_del .= $sep.substr($line, 1); 
         $sep         = ' / '; } 
       else
-        $sep         = ' […] '; } }
+        $sep         = ' […] '; }
   $string_del = substr($string_del, 6);
 
   # Build summary string by adding everything; reduce to 100 UTF8 chars.
