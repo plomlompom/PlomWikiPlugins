e28d2d3b1b3ac8574f86a4a549546b2d474f5a66
Christian Heller <c.heller@plomlompom.de>
2012-02-15 04:30:20 +0100

    Small formatting change in AutoSum.php.

--- AutoSum.php
+++ AutoSum.php
@@ -1,14 +1,14 @@
 <?php
 # PlomWiki plugin: AutoSum
 # 
-# Provides to the diff summary field an automatic summary of what was changed,
-# by excerpting the actual diff text.
+# Provides to the diff summary field an automatic summary of what was
+# changed, by excerpting the actual diff text.
 
 # Hook into WritePage() to fill up an empty $summary with AutoSum().
 $hook_WritePage_diff .= 'if (!$summary) $summary = AutoSum($diff_add);';
 
 function AutoSum($diff_add)
-# Build a nice short single-line summary of a diff from diff text provided.
+# Build nice short single-line summary of diff from diff text provided.
 { global $nl;
   $diff_lines = explode($nl, $diff_add); 
 
@@ -41,9 +41,9 @@ function AutoSum($diff_add)
         $sep         = ' […] '; } }
   $string_del = substr($string_del, 6);
 
-  # Build whole summary string by adding everything; cut down to 100 UTF8 chars.
-  $summary = 'AutoSum: '.$prefix_add.$string_add.$sep_to_minus.$prefix_del.
-                                                                    $string_del;
+  # Build summary string by adding everything; reduce to 100 UTF8 chars.
+  $summary = 'AutoSum: '.$prefix_add.$string_add.$sep_to_minus.
+                                                $prefix_del.$string_del;
   if (100 < strlen($summary))
     $summary = mb_substr($summary, 0, 99, 'UTF-8').'…';
   
