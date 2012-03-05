ece845eafa5fe29f2bf9be84494871e6623247fc
Christian Heller <c.heller@plomlompom.de>
2012-03-05 06:57:04 +0100

    Completely re-wrote PlomDesign plugin.

--- PlomDesign.php
+++ PlomDesign.php
@@ -3,19 +3,22 @@
 $s = ReadStringsFile($plugin_strings_dir.'PlomDesign', $s);
 
 $hook_before_action .= '
-if (substr($action, 7, 5) !== "page_")
-  $s["ActionLinks_page"] = "";
 if ($action == "Action_page_view") {
-  if (is_file($page_path))
-    $s["css_class"]        = "default article";
-  $s["design"]          .= Comments();
-  $s["design"]          .= Autolink_Backlinks(); }
-if ($action == "Action_page_edit") { 
-  $s["css_class"]        = "edit default"; }
-if ($action == "Action_page_history")
-  $s["css_class"]        = "default history";
+  $s["design"]    .= Comments();
+  $s["design"]    .= Autolink_Backlinks();
+  $s["css"]        = $s["css_pageview"];
+  $s["css_class"] .= " pageview"; }
+else if ($action == "Action_page_history")
+  $s["css"]         .= $s["css_history"];
+else if ($action == "Action_page_edit")
+  $s["css"]         .= $s["css_edit"];
+else if (substr($action, 7, 5) !== "page_")
+  $s["ActionLinks_page"] = "";
 ';
 
 $hook_ErrorFail .= '
-$s["css_class"] = "default fail";
+$s["css_class"] .= " fail";
+$s["css"]        = $s["css_fail"];
+if ($msg = "IllegalPageTitle")
+  $s["ActionLinks_page"] = "";
 ';
