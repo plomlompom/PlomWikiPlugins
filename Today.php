1297c7b6ee371b204951d2a549fd8d8863c1001d
Christian Heller <c.heller@plomlompom.de>
2012-03-20 04:12:36 +0100

    Started AGPLv3-licensing plugins by providing LICENSE file and adding license notice, first to Today.php.

--- Today.php
+++ Today.php
@@ -1,7 +1,8 @@
 <?php
-# PlomWiki plugin: Today markup
+# PlomWiki plugin: Today. Markup [:today:] displays current Y-m-d date.
 # 
-# Provides [:today:] markup, displays current date in format "Y-m-d".
+# Copyright 2010-2012 Christian Heller / <http://www.plomlompom.de/>
+# License: AGPLv3 or any later version. See file LICENSE for details.
 
 function MarkupToday($text) {
 # Translates [:today:] into the current date, formatted as Y-m-d.
