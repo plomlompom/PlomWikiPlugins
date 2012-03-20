0b04d58717807451419b3af5bf13e2dbb03e6a8a
Christian Heller <c.heller@plomlompom.de>
2012-03-20 07:42:57 +0100

    Licensed and re-designed PlomDesign plugin.

--- PlomDesign.php
+++ PlomDesign.php
@@ -1,27 +1,31 @@
 <?php
+# PlomWiki plugin: PlomDesign. Design/layout of <www.plomlompom.de>.
+# 
+# Copyright 2010-2012 Christian Heller / <http://www.plomlompom.de/>
+# License: AGPLv3 or any later version. See file LICENSE for details.
 
 $s = ReadStringsFile($plugin_strings_dir.'PlomDesign', $s);
 
 $hook_before_action .= '
 if ($action == "Action_page_view") {
-  $s["design"]    .= Comments();
-  $s["design"]    .= Autolink_Backlinks();
-  $s["css"]        = $s["css_pageview"];
-  $s["css_class"] .= " pageview"; }
+  $s["PlomDesign_css"]   = $s["PlomDesign_css_pageview"];
+  $s["css_class"]       .= " pageview";
+}
 else if ($action == "Action_page_history")
-  $s["css"]         .= $s["css_history"];
+  $s["PlomDesign_css"]  .= $s["css_history"];
 else if ($action == "Action_page_edit")
-  $s["css"]         .= $s["css_edit"];
+  $s["PlomDesign_css"]  .= $s["css_edit"];
 else if (substr($action, 7, 5) !== "page_")
-  $s["ActionLinks_page"] = "";
+  $s["PlomDesign_ActionLinks"] = "";
 if (   $action == "Action_page_Comments_hidden"
     or $action == "Action_page_Comments_mod")
-  $s["css"] .= $s["css_comments"];
+  $s["css_class"]       .= " Comments";
+  $s["PlomDesign_css"]  .= $s["PlomDesign_css_Comments"];
 ';
 
 $hook_ErrorFail .= '
 $s["css_class"] .= " fail";
-$s["css"]        = $s["css_fail"];
+$s["PlomDesign_css"]     = $s["PlomDesign_css_fail"];
 if ($msg = "IllegalPageTitle")
-  $s["ActionLinks_page"] = "";
+  $s["PlomDesign_ActionLinks"] = "";
 ';
